80f943bc1e69c38f559144196b7f35096fdf0e7d
Alexander Chernyh <alexchernyh2000@mail.ru>
2023-11-28 16:03:32 +0300

    first commit

---
+++ src/stores/MoviStore.js
@@ -0,0 +1,9 @@
+import { defineStor, defineStore } from "pinia"
+
+export const useMovieStore = defineStore('movieStore', {
+    state: () => {
+        movies: {
+            
+        }
+    }
+})
